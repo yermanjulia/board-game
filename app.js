7714cfa0b557713567277fa2e7f4fcfd76e0efb2
Julia Yerman <juliyaerm88@gmail.com>
2023-07-10 20:55:09 -0700

    Crafted board-game

---
+++ app.js
@@ -0,0 +1,36 @@
+const board = document.querySelector("#board");
+const colors = [
+  "#ff00c9",
+  "#cbf09f",
+  "#1eb7f2",
+  "#01c45b",
+  "#ec5073",
+  "#f9e81f",
+];
+const SQUARES_NUMBER = 500;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => setColor(square));
+  square.addEventListener("mouseleave", () => removeColor(square));
+
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px $(color)`;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = "#1d1d1d";
+  element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
